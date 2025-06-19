84af311833ee75afa226f2ad58e1d70c499dcc47
adrik27 <adrikmuhammad0@gmail.com>
2025-06-19 21:45:13 +0700

    templating

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthenticateController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/login', [AuthenticateController::class, 'tampil_login']);
 });
