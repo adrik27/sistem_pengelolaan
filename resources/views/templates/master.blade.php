6c262e4a0578ab1f626e5fa8f7641c80e8dc15cc
adrik27 <adrikmuhammad0@gmail.com>
2025-06-23 22:37:43 +0700

    transaksi masuk = crud

--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -5,12 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="description" content="Responsive HTML Admin Dashboard Template based on Bootstrap 5">
-    <meta name="author" content="NobleUI">
-    <meta name="keywords"
-        content="nobleui, bootstrap, bootstrap 5, bootstrap5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
+    <meta name="description" content="Sistem Pengelolaan Stok dan Keuangan">
+    <meta name="author" content="MT">
 
-    <title>NobleUI - HTML Bootstrap 5 Admin Dashboard Template</title>
+    <title>STOKEU - Sistem Pengelolaan Stok dan Keuangan</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
