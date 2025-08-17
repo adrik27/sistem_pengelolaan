225832b8622a788dedfb467864628b5c7f0ebdcd
adrik27 <adrikmuhammad0@gmail.com>
2025-08-17 23:03:41 +0700

    update api penerimaan = create,edit,delete (clear)

--- resources/views/Admin/Transaksi/tampil_transaksi_keluar.blade.php
+++ resources/views/Admin/Transaksi/tampil_transaksi_keluar.blade.php
@@ -676,6 +676,16 @@
                     cancelButtonText: "Batal"
                 }).then((result) => {
                     if (result.isConfirmed) {
+                        // Tampilkan spinner loading
+                        Swal.fire({
+                            title: 'Proses Tambah Data...',
+                            text: 'Mohon tunggu sebentar',
+                            allowOutsideClick: false,
+                            didOpen: () => {
+                                Swal.showLoading();
+                            }
+                        });
+
                         form.submit();
                     }
                 });
